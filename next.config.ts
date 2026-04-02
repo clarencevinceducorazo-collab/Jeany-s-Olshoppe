817827e881a0bc21071fc9f4e53615dff8b300fc
clarencevinceducorazo-collab <clarencevinceducorazo@gmail.com>
2026-04-02 13:55:28 +0800

    updated

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'platform-lookaside.fbsbx.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
