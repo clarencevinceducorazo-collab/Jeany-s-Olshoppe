6ffdeded33e2104ae29e98bdc1df144b97835a20
clarencevinceducorazo-collab <clarencevinceducorazo@gmail.com>
2026-04-12 22:21:35 +0800

    updated next.config

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'gzswhhywwunktowebydw.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
     ],
   },
 };
