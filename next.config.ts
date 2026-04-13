4246d213f718318c68f77a2d0e455e80a47efc58
clarencevinceducorazo-collab <clarencevinceducorazo@gmail.com>
2026-04-13 23:18:47 +0800

    updated config

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,8 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '100mb',
-    },
+  serverActions: {
+    bodySizeLimit: '100mb',
   },
   typescript: {
     ignoreBuildErrors: true,
