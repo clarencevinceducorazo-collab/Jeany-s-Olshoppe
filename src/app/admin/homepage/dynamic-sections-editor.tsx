dc78a1752780114d6f01ee65d9ebc2c3b6e6caad
clarencevinceducorazo-collab <clarencevinceducorazo@gmail.com>
2026-04-14 22:51:21 +0800

    fixed error in dynamic sections

--- src/app/admin/homepage/dynamic-sections-editor.tsx
+++ src/app/admin/homepage/dynamic-sections-editor.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useTransition } from 'react'
 import { useToast } from '@/hooks/use-toast'
-import { createHomeSection, updateHomeSection, deleteHomeSection, reorderHomeSection } from '../actions'
+import { createHomeSection, updateHomeSection, deleteHomeSection, reorderHomeSection } from './actions'
 import { Label } from '@/components/ui/label'
 import { Input } from '@/components/ui/input'
 import { Textarea } from '@/components/ui/textarea'
